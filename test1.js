e6634293a1590f6f45d52511b4b23655f7c46378
stonecoldshree <shreesujal1405@gmail.com>
2026-01-20 18:02:30 +0530

    test1

---
+++ test1.js
@@ -0,0 +1,5 @@
+const account_Id=144;
+let account_Email="shree@gmail.com";
+let account_Password="Shree@1234";
+let account_City="Banglore";
+console.table([account_Id,account_Email,account_Password,account_City]);
